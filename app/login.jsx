d5686074914a3e5f95d045e17819def28af3de02
aryanhedaoo22072003 <aryanhedaoo2003@gmail.com>
2024-11-22 15:24:36 +0530

    Signup page

--- app/login.jsx
+++ app/login.jsx
@@ -63,7 +63,7 @@ const Login = () => {
           <Text style={styles.footerText}>
             Don't have an account?
           </Text>
-          <Pressable>
+          <Pressable onPress={()=>router.push('signUp')}>
             <Text style={[styles.footerText,{color:theme.colors.primaryDark,fontWeight:theme.fonts.semibold}]}>Sign up</Text>
           </Pressable>
         </View>
