40aba6221a811bf88ccda31fd41e279d13551c9f
aryanhedaoo22072003 <aryanhedaoo2003@gmail.com>
2024-11-28 15:13:47 +0530

    Home style and database

--- app/(main)/home.jsx
+++ app/(main)/home.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import ScreenWrapper from '../../components/ScreenWrapper'
 import { useAuth } from '../../contexts/AuthContext'
 import { supabase } from '../../lib/supabase'
+import { hp, wp } from '../../helpers/common'
+import { theme } from '../../constants/theme'
 
 const Home = () => {
     const {user,seAuth}=useAuth();
@@ -26,4 +28,59 @@ const Home = () => {
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header:{
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'space-between',
+    marginBottom:10,
+    marginHorizontal:wp(4)
+  },
+  title:{
+    color:theme.colors.text,
+    fontSize:hp(3.2),
+    fontWeight:theme.fonts.bold
+  },
+  avatarImage:{
+    height:hp(4.3),
+    width:hp(4.3),
+    borderRadius:theme.radius.sm,
+    borderCurve:'continuous',
+    borderColor:theme.colors.gray,
+    borderWidth:3
+  },
+  icons:{
+    flexDirection:'row',
+    justifyContent:'center',
+    alignItems:'center',
+    gap:18
+  },
+  listStyle:{
+    paddingTop:20,
+    paddingHorizontal:wp(4)
+  },
+  noPosts:{
+    fontSize:hp(2),
+    textAlign:'center',
+    color:theme.colors.text
+  },
+  pill:{
+    position:'absolute',
+    right:-10,
+    top:-4,
+    height:hp(2.2),
+    width:hp(2.2),
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:20,
+    backgroundColor:theme.colors.roseLight
+  },
+  pillText:{
+    color:'white',
+    fontSize:hp(1.2),
+    fontWeight:theme.fonts.bold
+  }
+})
