050efdab88241458733e98ea893fe8b0de6a3ea6
aryanhedaoo22072003 <aryanhedaoo2003@gmail.com>
2024-12-02 17:37:33 +0530

    New Post

--- app/(main)/newPost.jsx
+++ app/(main)/newPost.jsx
@@ -5,10 +5,43 @@ import Header from '../../components/Header'
 import { hp, wp } from '../../helpers/common'
 import { theme } from '../../constants/theme'
 
+import Avatar from '../../components/Avatar'
+import { useAuth } from '../../contexts/AuthContext'
+import { ScrollView } from 'react-native'
+import RichTextEditor from '../../components/RichTextEditor'
+
 const NewPost = () => {
+  const {user}=useAuth();
   return (
-    <ScreenWrapper>
-      <Header title="Create Post" />
+    <ScreenWrapper bg="white">
+      <View style={styles.container}>
+       <Header title="Create Post" />
+       <ScrollView contentContainerStyle={{gap:20}}>
+        {/* avatar */}
+        <View style={styles.header}>
+          <Avatar 
+             uri={user?.image}
+             size={hp(6.5)}
+             rounded={theme.radius.xl}
+          />
+          <View style={{gap:2}}>
+            <Text style={styles.username}>
+              {
+                user && user.name
+              }
+            </Text>
+            <Text style={styles.publicText}>
+              public
+            </Text>
+          </View>
+        </View>
+        
+        <View style={styles.textEditor}>
+          <RichTextEditor />
+        </View>
+       </ScrollView>
+      </View>
+      
     </ScreenWrapper>
   )
 }
