07eb785b7d04c6910ccbe627f5497ac28fb30c75
aryanhedaoo22072003 <aryanhedaoo2003@gmail.com>
2024-12-03 16:28:26 +0530

    New Post

--- app/(main)/newPost.jsx
+++ app/(main)/newPost.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useRef, useState } from 'react'
 import ScreenWrapper from '../../components/ScreenWrapper'
 import Header from '../../components/Header'
@@ -10,7 +10,10 @@ import { useAuth } from '../../contexts/AuthContext'
 import { ScrollView } from 'react-native'
 import RichTextEditor from '../../components/RichTextEditor'
 import { useRouter } from 'expo-router'
-
+import Icon from '../../assets/icons'
+import Button from '../../components/Button'
+import * as ImagePicker from 'expo-image-picker';
+ 
 const NewPost = () => {
   const {user}=useAuth();
 
@@ -19,6 +22,49 @@ const NewPost = () => {
   const router=useRouter();
   const [loading,setloading]=useState(false);
   const [file,setFile]=useState(file);
+
+  const onPick=async(isImage)=>{
+    let mediaConfig={
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing:true,
+      aspect:[4,3],
+      quality:0.7,
+    }
+    if(!isImage){
+      mediaConfig={
+        mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+        allowsEditing:true,
+      }
+    }
+    let result=await ImagePicker.launchImageLibraryAsync(mediaConfig);
+
+    if(!result.canceled){
+      setFile(result.assets[0]);
+    }
+  }
+  const isLocalFile=file=>{
+    if(!file) return null;
+    if(typeof file=='object') return true;
+
+    return false;
+  } 
+  const getFileType=file =>{
+    if(!file) return null;
+    if(isLocalFile(file)){
+      return file.type;
+    }
+
+    //check image or video for remote file
+    if(file.includes('postImage')){
+      return 'image';
+    }
+    return 'video';
+  }
+
+  const onSubmit=async()=>{
+
+  }
+
   return (
     <ScreenWrapper bg="white">
       <View style={styles.container}>
@@ -46,7 +92,44 @@ const NewPost = () => {
         <View style={styles.textEditor}>
           <RichTextEditor  editorRef={editorRef} onChange={body=>bodyRef.current=body}/>
         </View>
+
+          {
+            file && (
+              <View style={styles.file}>
+                {
+                  getFileType(file)== 'video'?(
+                    <></>
+                  ):(
+                    <></>
+                  )
+
+                }
+              </View>
+            )
+          }
+
+
+        <View style={styles.media}>
+          <Text style={styles.addImageText}>Add to your post</Text>
+          <View style={styles.mediaIcons}>
+            <TouchableOpacity onPress={()=>onPick(true)}>
+              <Icon name="image" size={30} color={theme.colors.dark} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=>onPick(false)}>
+              <Icon name="video" size={33} color={theme.colors.dark} />
+            </TouchableOpacity>
+          </View>
+         
+        </View>
        </ScrollView>
+       <Button 
+            buttonStyle={{height:hp(6.2)}}
+            title='Post'
+            loading={loading}
+            hasShadow={false}
+            onPress={onSubmit}
+
+        />
       </View>
       
     </ScreenWrapper>
