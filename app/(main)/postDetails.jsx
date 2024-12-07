cd421a5c95ca19b41a1fb3d72280de1158068f6b
aryanhedaoo22072003 <aryanhedaoo2003@gmail.com>
2024-12-07 15:22:17 +0530

    Like,share and comment post

--- app/(main)/postDetails.jsx
+++ app/(main)/postDetails.jsx
@@ -1,10 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
+import {StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { useLocalSearchParams, useRouter } from 'expo-router'
 import { fetchPostDetails } from '../../services/postService';
+import { hp, wp } from '../../helpers/common';
+import { theme } from '../../constants/theme';
+import PostCard from '../../components/PostCard';
+import { useAuth } from '../../contexts/AuthContext';
+import Loading from '../../components/Loading';
+import { ScrollView } from 'react-native';
+
 
 const PostDetails = () => {
     const {postId}=useLocalSearchParams();
+    const {user}=useAuth();
+    const router=useRouter();
+    const [startLoading,setStartLoading]=useState(true);
     console.log('got post Id :',postId);
 
     const [post, setPost] = useState(null);
@@ -16,16 +26,74 @@ const PostDetails = () => {
     const getPostDetails=async()=>{
         //fetch post details here
         let res=await fetchPostDetails(postId);
-        console.log('got post details:',res);
+        if(res.success) setPost(res.data);
+        setStartLoading(false);
+    }
+
+    if(startLoading){
+        return(
+            <View style={styles.center}>
+                <Loading/>
+            </View>
+        )
     }
 
   return (
-    <View>
-      <Text>PostDetails</Text>
+    <View style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.list}>
+        <PostCard
+            item={post}
+            currentUser={user}
+            router={router}
+            hasShadow={false}
+        />
+      </ScrollView>
     </View>
   )
 }
 
 export default PostDetails
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'white',
+        paddingVertical:wp(7),
+    },
+    inputContainer:{
+        flexDirection:'row',
+        alignItems:'center',
+        gap:10
+    },
+    list:{
+        paddingHorizontal:wp(4)
+    },
+    sendIcon:{
+        alignItems:'center',
+        justifyContent:'center',
+        borderWidth:0.8,
+        borderColor:theme.colors.primary,
+        borderRadius:theme.radius.lg,
+        borderCurve:'continuous',
+        height:hp(5.8),
+        width:hp(5.8)
+    },
+    center:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    notFound:{
+        fontSize:hp(2.5),
+        color:theme.colors.text,
+        fontWeight:theme.fonts.medium,
+    },
+    loading:{
+        height:hp(5.8),
+        width:hp(5.8),
+        justifyContent:'center',
+        alignItems:'center',
+        transform:[{scale:1.3}]
+    }
+
+})
